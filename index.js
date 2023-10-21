d785384ebcab4004f912e313954f7bb6ea46ccf2
Shuvo0414 <138393384+Shuvo0414@users.noreply.github.com>
2023-10-21 16:29:04 +0600

    feat: Implement cart item deletion in the server-side API

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5001;
 
@@ -75,6 +75,13 @@ async function run() {
       const addCartProduct = req.body;
       console.log(addCartProduct);
       const result = await cartCollection.insertOne(addCartProduct);
+      res.send(result);
+    });
+
+    app.delete("/addToCart/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
       res.send(result);
     });
 
